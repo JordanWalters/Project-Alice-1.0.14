cbacf68fd3e2dd38f1e56a83b3588072f0c2f8e1
JordanWalters <jordanleawalters@gmail.com>
2025-03-27 14:46:50 +0000

    Fixed base code & added relations limit for real this time

--- src/ai/ai.cpp
+++ src/ai/ai.cpp
@@ -281,8 +281,9 @@ bool ai_will_accept_alliance(sys::state& state, dcon::nation_id target, dcon::na
 	if(!state.world.nation_get_ai_is_threatened(target))
 		return false;
 
-	//Has more than 0 relations
-	if(state.world.get_diplomatic_relation_by_diplomatic_pair(n, target) <= 0)
+		//Has more than 0 relations
+	auto rel_x = state.world.get_diplomatic_relation_by_diplomatic_pair(from, target);
+	if(state.world.diplomatic_relation_get_value(rel_x) < 0)
 		return false;
 
 	// Has not surpassed infamy limit
@@ -293,13 +294,13 @@ bool ai_will_accept_alliance(sys::state& state, dcon::nation_id target, dcon::na
 	if(state.world.nation_get_ai_rival(target) == from || state.world.nation_get_ai_rival(from) == target)
 		return false;
 
-	if(bool(state.defines.alice_artificial_gp_limitant) && state.world.nation_get_is_great_power(target)) {
-		int32_t gp_count = 0;
+	int32_t gp_count = 0;
+	if(bool(state.world.nation_get_is_great_power(target))) {
 		for(const auto rel : state.world.nation_get_diplomatic_relation(from)) {
 			auto n = rel.get_related_nations(rel.get_related_nations(0) == from ? 1 : 0);
 			if(rel.get_are_allied() && n.get_is_great_power()) {
 				++gp_count;
-				if(gp_count >= 2) {
+				if(gp_count >= 1) {
 					return false;
 				}
 			}
